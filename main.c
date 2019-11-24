9149d72b13025bfa330dda948556d2f82adca379
Live session user <roi2307@gmail.com>
2019-11-24 13:34:10 +0000

    fixed double printing and add half of functions

--- main.c
+++ main.c
@@ -2,47 +2,46 @@
 #include <stdio.h>
 int main()
 {
-    char input;
+    char input ='X';
     double amount = 0.0;
     int id = -1;
-    printf("Transaction type?:");
-    scanf("%c", &input);
+    init();
     while (input != 'E')
     {
         switch (input)
         {
         case 'O':
             printf("Initial deposit?:");
-            scanf("%lf", &amount);
+            scanf(" %lf", &amount);
             newAccount(amount);
             break;
         case 'B':
             printf("Account number:");
-            scanf("%d", &id);
+            scanf(" %d", &id);
             getBalance(id);
             break;
         case 'D':
             printf("Account number:");
-            scanf("%d", &id);
+            scanf(" %d", &id);
             printf("Amount?:");
-            scanf("%lf", &amount);
+            scanf(" %lf", &amount);
             deposit(id, amount);
             break;
         case 'W':
             printf("Account number:");
-            scanf("%d", &id);
+            scanf(" %d", &id);
             printf("Amount?:");
-            scanf("%lf", &amount);
+            scanf(" %lf", &amount);
             withdraw(id, amount);
             break;
         case 'C':
             printf("Account number:");
-            scanf("%d", &id);
+            scanf(" %d", &id);
             shut(id);
             break;
         case 'I':
             printf("Interest rate?:");
-            scanf("%lf", &amount);
+            scanf(" %lf", &amount);
             addInterest(amount);
             break;
         case 'P':
@@ -50,7 +49,7 @@ int main()
             break;
         }
         printf("Transaction type?:");
-        scanf("%c", &input);
+        scanf(" %c", &input);
     }
     return 0;
 }
--- myBank.c
+++ myBank.c
@@ -1,10 +1,45 @@
 #include "myBank.h"
+#include <stdio.h>
 
-double acc[2][50];
-void newAccount(double amount){;}
-void getBalance(int id){;}
-void deposit(int id, double amount){;}
-void withdraw(int id, double amount){;}
-void shut(int id){;}
-void addInterest(double prec){;}
-void report(){;}
+double acc[1][49];
+int nextFree = 0;
+void init()
+{
+    for (int i = 0; i < nextFree; i++)
+    {
+        acc[0][i] = 0.0;
+        acc[1][i] = 0.0;
+    }
+}
+void newAccount(double amount)
+{
+    if (nextFree < 49)
+    {
+        acc[0][nextFree] = 1;
+        acc[1][nextFree] = amount;
+        nextFree++;
+    }
+}
+void getBalance(int id)
+{
+    if (acc[0][id] == 0.0)
+    {
+        printf("closed account /n");
+    }
+    else
+    {
+        printf("Balance: %lf", acc[1][id]);
+    }
+}
+void deposit(int id, double amount) { ; }
+void withdraw(int id, double amount) { ; }
+void shut(int id) { ; }
+void addInterest(double prec) { ; }
+void report()
+{
+    for (int i = 0; i < nextFree; i++)
+    {
+        printf("val acc[1][%d] = %lf", i, acc[1][i]);
+        printf("\n");
+    }
+}
